1e41fe1b1705cda75e486e2194c6398e9f0f2c52
Aileen <asantos3026@gmail.com>
2017-04-11 22:20:46 -0700

    Add length to insertBefore

--- src/linked_list.js
+++ src/linked_list.js
@@ -19,14 +19,14 @@ class LinkedList {
     if(!value){
       throw new Error('Please pass in a value')
     }
-    //case 2 no head, make the new node the new head 
+    //case 2 no head, make the new node the new head
     if(!this.head) {
       this.head = node
       this._length++
 
       return node
     }
-    //case 3, head exists,  
+    //case 3, head exists,
     let currentNode = this.head
     while(currentNode.next) {
       currentNode = currentNode.next
@@ -59,18 +59,21 @@ class LinkedList {
     while( (currentNode !== null) && (insertedNode === undefined) ) {
       if( JSON.stringify(currentNode.next.data) === JSON.stringify(data) ){
        insertedNode = new Node( newData )
+       // make inserted node's next the old currentNode's next
        insertedNode.next = currentNode.next
+       // set the new inserted node as the old currentNode's next
        currentNode.next = insertedNode
+       this._length++
       }
       if( (currentNode === this.head) && (currentNode.data === data) ) {
         insertedNode = new Node( newData )
         insertedNode.next = currentNode
         this.head = insertedNode
+        this._length++
       }
     }
     return insertedNode
   }
-
 
   insertAfter( data, newData ) {
     let node = new Node( newData )
@@ -85,7 +88,7 @@ class LinkedList {
       currentNode = currentNode.next
     }
   }
-  
+
   getHeadNode() {
     return this.head
   }
@@ -97,6 +100,17 @@ class LinkedList {
     }
     return currentNode
   }
+
+  size() {
+    return this._length
+  }
 }
 
-export default LinkedList
+const ll = new LinkedList()
+ll.insert(1)
+ll.insert(3)
+ll.insert(4)
+ll.insertBefore(3, 2)
+console.log(ll)
+
+// export default LinkedList
